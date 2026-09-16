f21607e453c0163c6a11f233a29355b47a018f7b
Zong <36668060+sqz269@users.noreply.github.com>
2026-09-16 03:31:12 -0700

    Keep raw clock instruction addresses as comments except branch targets

--- src/native_frame_clock_actual.cpp
+++ src/native_frame_clock_actual.cpp
@@ -58,144 +58,144 @@ std::int64_t __stdcall fixed_increment(std::int64_t frequency, std::int32_t mill
 // Only the QPC import and five timestamp-subtract calls are relocated.
 __declspec(naked) void __fastcall update_native_frame_clock_00bedc30(void*) {
     __asm {
-    label_00bedc30: SUB ESP,0x28
-    label_00bedc33: PUSH ESI
-    label_00bedc34: MOV ESI,ECX
-    label_00bedc36: CMP byte ptr [ESI + 0x68],0x0
-    label_00bedc3a: JZ label_00bedc67
-    label_00bedc3c: ADD ESI,0x40
-    label_00bedc3f: PUSH ESI
-    label_00bedc40: LEA EAX,[ESP + 0x10]
-    label_00bedc44: PUSH EAX
-    label_00bedc45: MOV ECX,ESI
-    label_00bedc47: CALL subtract_raw
-    label_00bedc4c: MOV ECX,dword ptr [EAX]
-    label_00bedc4e: MOV dword ptr [ESI],ECX
-    label_00bedc50: MOV EDX,dword ptr [EAX + 0x4]
-    label_00bedc53: MOV dword ptr [ESI + 0x4],EDX
-    label_00bedc56: MOV ECX,dword ptr [EAX + 0x8]
-    label_00bedc59: MOV dword ptr [ESI + 0x8],ECX
-    label_00bedc5c: MOV EDX,dword ptr [EAX + 0xc]
-    label_00bedc5f: MOV dword ptr [ESI + 0xc],EDX
-    label_00bedc62: POP ESI
-    label_00bedc63: ADD ESP,0x28
-    label_00bedc66: RET
+        SUB ESP,0x28 // 00bedc30
+        PUSH ESI // 00bedc33
+        MOV ESI,ECX // 00bedc34
+        CMP byte ptr [ESI + 0x68],0x0 // 00bedc36
+        JZ label_00bedc67 // 00bedc3a
+        ADD ESI,0x40 // 00bedc3c
+        PUSH ESI // 00bedc3f
+        LEA EAX,[ESP + 0x10] // 00bedc40
+        PUSH EAX // 00bedc44
+        MOV ECX,ESI // 00bedc45
+        CALL subtract_raw // 00bedc47
+        MOV ECX,dword ptr [EAX] // 00bedc4c
+        MOV dword ptr [ESI],ECX // 00bedc4e
+        MOV EDX,dword ptr [EAX + 0x4] // 00bedc50
+        MOV dword ptr [ESI + 0x4],EDX // 00bedc53
+        MOV ECX,dword ptr [EAX + 0x8] // 00bedc56
+        MOV dword ptr [ESI + 0x8],ECX // 00bedc59
+        MOV EDX,dword ptr [EAX + 0xc] // 00bedc5c
+        MOV dword ptr [ESI + 0xc],EDX // 00bedc5f
+        POP ESI // 00bedc62
+        ADD ESP,0x28 // 00bedc63
+        RET // 00bedc66
     label_00bedc67: ADD dword ptr [ESI + 0x8],0x1
-    label_00bedc6b: MOV EAX,dword ptr [ESI + 0x20]
-    label_00bedc6e: MOV ECX,dword ptr [ESI + 0x24]
-    label_00bedc71: MOV EDX,dword ptr [ESI + 0x28]
-    label_00bedc74: ADC dword ptr [ESI + 0xc],0x0
-    label_00bedc78: CMP byte ptr [ESI + 0x69],0x0
-    label_00bedc7c: PUSH EBX
-    label_00bedc7d: PUSH EDI
-    label_00bedc7e: LEA EDI,[ESI + 0x20]
-    label_00bedc81: LEA EBX,[ESI + 0x30]
-    label_00bedc84: MOV dword ptr [EBX],EAX
-    label_00bedc86: MOV EAX,dword ptr [EDI + 0xc]
-    label_00bedc89: MOV dword ptr [EBX + 0x4],ECX
-    label_00bedc8c: MOV dword ptr [EBX + 0x8],EDX
-    label_00bedc8f: MOV dword ptr [EBX + 0xc],EAX
-    label_00bedc92: JNZ label_00bedce7
-    label_00bedc94: LEA ECX,[ESP + 0xc]
-    label_00bedc98: PUSH ECX
-    label_00bedc99: CALL QueryPerformanceCounter
-    label_00bedc9f: MOV EAX,dword ptr [ESP + 0x10]
-    label_00bedca3: MOV ECX,dword ptr [ESI + 0x60]
-    label_00bedca6: MOV EDX,dword ptr [ESP + 0xc]
-    label_00bedcaa: MOV dword ptr [ESP + 0x18],EAX
-    label_00bedcae: MOV dword ptr [ESP + 0x1c],ECX
-    label_00bedcb2: LEA EAX,[ESI + 0x10]
-    label_00bedcb5: PUSH EAX
-    label_00bedcb6: LEA ECX,[ESP + 0x28]
-    label_00bedcba: MOV dword ptr [ESP + 0x18],EDX
-    label_00bedcbe: MOV EDX,dword ptr [ESI + 0x64]
-    label_00bedcc1: PUSH ECX
-    label_00bedcc2: LEA ECX,[ESP + 0x1c]
-    label_00bedcc6: MOV dword ptr [ESP + 0x28],EDX
-    label_00bedcca: CALL subtract_raw
-    label_00bedccf: MOV EDX,dword ptr [EAX]
-    label_00bedcd1: MOV dword ptr [EDI],EDX
-    label_00bedcd3: MOV ECX,dword ptr [EAX + 0x4]
-    label_00bedcd6: MOV dword ptr [EDI + 0x4],ECX
-    label_00bedcd9: MOV EDX,dword ptr [EAX + 0x8]
-    label_00bedcdc: MOV dword ptr [EDI + 0x8],EDX
-    label_00bedcdf: MOV EAX,dword ptr [EAX + 0xc]
-    label_00bedce2: MOV dword ptr [EDI + 0xc],EAX
-    label_00bedce5: JMP label_00bedd37
+        MOV EAX,dword ptr [ESI + 0x20] // 00bedc6b
+        MOV ECX,dword ptr [ESI + 0x24] // 00bedc6e
+        MOV EDX,dword ptr [ESI + 0x28] // 00bedc71
+        ADC dword ptr [ESI + 0xc],0x0 // 00bedc74
+        CMP byte ptr [ESI + 0x69],0x0 // 00bedc78
+        PUSH EBX // 00bedc7c
+        PUSH EDI // 00bedc7d
+        LEA EDI,[ESI + 0x20] // 00bedc7e
+        LEA EBX,[ESI + 0x30] // 00bedc81
+        MOV dword ptr [EBX],EAX // 00bedc84
+        MOV EAX,dword ptr [EDI + 0xc] // 00bedc86
+        MOV dword ptr [EBX + 0x4],ECX // 00bedc89
+        MOV dword ptr [EBX + 0x8],EDX // 00bedc8c
+        MOV dword ptr [EBX + 0xc],EAX // 00bedc8f
+        JNZ label_00bedce7 // 00bedc92
+        LEA ECX,[ESP + 0xc] // 00bedc94
+        PUSH ECX // 00bedc98
+        CALL QueryPerformanceCounter // 00bedc99
+        MOV EAX,dword ptr [ESP + 0x10] // 00bedc9f
+        MOV ECX,dword ptr [ESI + 0x60] // 00bedca3
+        MOV EDX,dword ptr [ESP + 0xc] // 00bedca6
+        MOV dword ptr [ESP + 0x18],EAX // 00bedcaa
+        MOV dword ptr [ESP + 0x1c],ECX // 00bedcae
+        LEA EAX,[ESI + 0x10] // 00bedcb2
+        PUSH EAX // 00bedcb5
+        LEA ECX,[ESP + 0x28] // 00bedcb6
+        MOV dword ptr [ESP + 0x18],EDX // 00bedcba
+        MOV EDX,dword ptr [ESI + 0x64] // 00bedcbe
+        PUSH ECX // 00bedcc1
+        LEA ECX,[ESP + 0x1c] // 00bedcc2
+        MOV dword ptr [ESP + 0x28],EDX // 00bedcc6
+        CALL subtract_raw // 00bedcca
+        MOV EDX,dword ptr [EAX] // 00bedccf
+        MOV dword ptr [EDI],EDX // 00bedcd1
+        MOV ECX,dword ptr [EAX + 0x4] // 00bedcd3
+        MOV dword ptr [EDI + 0x4],ECX // 00bedcd6
+        MOV EDX,dword ptr [EAX + 0x8] // 00bedcd9
+        MOV dword ptr [EDI + 0x8],EDX // 00bedcdc
+        MOV EAX,dword ptr [EAX + 0xc] // 00bedcdf
+        MOV dword ptr [EDI + 0xc],EAX // 00bedce2
+        JMP label_00bedd37 // 00bedce5
     label_00bedce7: MOV ECX,dword ptr [ESI + 0x70]
-    label_00bedcea: ADD dword ptr [ESI + 0x78],ECX
-    label_00bedced: MOV EDX,dword ptr [ESI + 0x74]
-    label_00bedcf0: MOV EAX,dword ptr [ESI + 0x78]
-    label_00bedcf3: ADC dword ptr [ESI + 0x7c],EDX
-    label_00bedcf6: MOV ECX,dword ptr [ESI + 0x7c]
-    label_00bedcf9: MOV EDX,dword ptr [ESI + 0x60]
-    label_00bedcfc: MOV dword ptr [ESP + 0x18],ECX
-    label_00bedd00: LEA ECX,[ESI + 0x10]
-    label_00bedd03: MOV dword ptr [ESP + 0x1c],EDX
-    label_00bedd07: PUSH ECX
-    label_00bedd08: MOV dword ptr [ESP + 0x18],EAX
-    label_00bedd0c: MOV EAX,dword ptr [ESI + 0x64]
-    label_00bedd0f: LEA EDX,[ESP + 0x28]
-    label_00bedd13: PUSH EDX
-    label_00bedd14: LEA ECX,[ESP + 0x1c]
-    label_00bedd18: MOV dword ptr [ESP + 0x28],EAX
-    label_00bedd1c: CALL subtract_raw
-    label_00bedd21: MOV ECX,dword ptr [EAX]
-    label_00bedd23: MOV dword ptr [EDI],ECX
-    label_00bedd25: MOV EDX,dword ptr [EAX + 0x4]
-    label_00bedd28: MOV dword ptr [EDI + 0x4],EDX
-    label_00bedd2b: MOV ECX,dword ptr [EAX + 0x8]
-    label_00bedd2e: MOV dword ptr [EDI + 0x8],ECX
-    label_00bedd31: MOV EDX,dword ptr [EAX + 0xc]
-    label_00bedd34: MOV dword ptr [EDI + 0xc],EDX
+        ADD dword ptr [ESI + 0x78],ECX // 00bedcea
+        MOV EDX,dword ptr [ESI + 0x74] // 00bedced
+        MOV EAX,dword ptr [ESI + 0x78] // 00bedcf0
+        ADC dword ptr [ESI + 0x7c],EDX // 00bedcf3
+        MOV ECX,dword ptr [ESI + 0x7c] // 00bedcf6
+        MOV EDX,dword ptr [ESI + 0x60] // 00bedcf9
+        MOV dword ptr [ESP + 0x18],ECX // 00bedcfc
+        LEA ECX,[ESI + 0x10] // 00bedd00
+        MOV dword ptr [ESP + 0x1c],EDX // 00bedd03
+        PUSH ECX // 00bedd07
+        MOV dword ptr [ESP + 0x18],EAX // 00bedd08
+        MOV EAX,dword ptr [ESI + 0x64] // 00bedd0c
+        LEA EDX,[ESP + 0x28] // 00bedd0f
+        PUSH EDX // 00bedd13
+        LEA ECX,[ESP + 0x1c] // 00bedd14
+        MOV dword ptr [ESP + 0x28],EAX // 00bedd18
+        CALL subtract_raw // 00bedd1c
+        MOV ECX,dword ptr [EAX] // 00bedd21
+        MOV dword ptr [EDI],ECX // 00bedd23
+        MOV EDX,dword ptr [EAX + 0x4] // 00bedd25
+        MOV dword ptr [EDI + 0x4],EDX // 00bedd28
+        MOV ECX,dword ptr [EAX + 0x8] // 00bedd2b
+        MOV dword ptr [EDI + 0x8],ECX // 00bedd2e
+        MOV EDX,dword ptr [EAX + 0xc] // 00bedd31
+        MOV dword ptr [EDI + 0xc],EDX // 00bedd34
     label_00bedd37: PUSH EBX
-    label_00bedd38: LEA EAX,[ESP + 0x28]
-    label_00bedd3c: PUSH EAX
-    label_00bedd3d: MOV ECX,EDI
-    label_00bedd3f: CALL subtract_raw
-    label_00bedd44: MOV ECX,dword ptr [EAX]
-    label_00bedd46: MOV dword ptr [ESI + 0x40],ECX
-    label_00bedd49: MOV EDX,dword ptr [EAX + 0x4]
-    label_00bedd4c: MOV dword ptr [ESI + 0x44],EDX
-    label_00bedd4f: MOV ECX,dword ptr [EAX + 0x8]
-    label_00bedd52: MOV dword ptr [ESI + 0x48],ECX
-    label_00bedd55: MOV EDX,dword ptr [EAX + 0xc]
-    label_00bedd58: MOV dword ptr [ESI + 0x4c],EDX
-    label_00bedd5b: FILD qword ptr [ESI + 0x40]
-    label_00bedd5e: FILD qword ptr [ESI + 0x48]
-    label_00bedd61: FDIVP ST(1),ST(0)
-    label_00bedd63: FSTP float ptr [ESP + 0xc]
-    label_00bedd67: FLD float ptr [ESP + 0xc]
-    label_00bedd6b: FLDZ
-    label_00bedd6d: FCOMIP ST(0),ST(1)
-    label_00bedd6f: FSTP ST(0)
-    label_00bedd71: JBE label_00beddad
-    label_00bedd73: MOV EAX,dword ptr [EBX]
-    label_00bedd75: MOV ECX,dword ptr [EBX + 0x4]
-    label_00bedd78: MOV EDX,dword ptr [EBX + 0x8]
-    label_00bedd7b: MOV dword ptr [EDI],EAX
-    label_00bedd7d: MOV EAX,dword ptr [EBX + 0xc]
-    label_00bedd80: MOV dword ptr [EDI + 0x4],ECX
-    label_00bedd83: PUSH EBX
-    label_00bedd84: LEA ECX,[ESP + 0x28]
-    label_00bedd88: PUSH ECX
-    label_00bedd89: MOV dword ptr [EDI + 0x8],EDX
-    label_00bedd8c: MOV ECX,EDI
-    label_00bedd8e: MOV dword ptr [EDI + 0xc],EAX
-    label_00bedd91: CALL subtract_raw
-    label_00bedd96: MOV EDX,dword ptr [EAX]
-    label_00bedd98: MOV dword ptr [ESI + 0x40],EDX
-    label_00bedd9b: MOV ECX,dword ptr [EAX + 0x4]
-    label_00bedd9e: MOV dword ptr [ESI + 0x44],ECX
-    label_00bedda1: MOV EDX,dword ptr [EAX + 0x8]
-    label_00bedda4: MOV dword ptr [ESI + 0x48],EDX
-    label_00bedda7: MOV EAX,dword ptr [EAX + 0xc]
-    label_00beddaa: MOV dword ptr [ESI + 0x4c],EAX
+        LEA EAX,[ESP + 0x28] // 00bedd38
+        PUSH EAX // 00bedd3c
+        MOV ECX,EDI // 00bedd3d
+        CALL subtract_raw // 00bedd3f
+        MOV ECX,dword ptr [EAX] // 00bedd44
+        MOV dword ptr [ESI + 0x40],ECX // 00bedd46
+        MOV EDX,dword ptr [EAX + 0x4] // 00bedd49
+        MOV dword ptr [ESI + 0x44],EDX // 00bedd4c
+        MOV ECX,dword ptr [EAX + 0x8] // 00bedd4f
+        MOV dword ptr [ESI + 0x48],ECX // 00bedd52
+        MOV EDX,dword ptr [EAX + 0xc] // 00bedd55
+        MOV dword ptr [ESI + 0x4c],EDX // 00bedd58
+        FILD qword ptr [ESI + 0x40] // 00bedd5b
+        FILD qword ptr [ESI + 0x48] // 00bedd5e
+        FDIVP ST(1),ST(0) // 00bedd61
+        FSTP float ptr [ESP + 0xc] // 00bedd63
+        FLD float ptr [ESP + 0xc] // 00bedd67
+        FLDZ // 00bedd6b
+        FCOMIP ST(0),ST(1) // 00bedd6d
+        FSTP ST(0) // 00bedd6f
+        JBE label_00beddad // 00bedd71
+        MOV EAX,dword ptr [EBX] // 00bedd73
+        MOV ECX,dword ptr [EBX + 0x4] // 00bedd75
+        MOV EDX,dword ptr [EBX + 0x8] // 00bedd78
+        MOV dword ptr [EDI],EAX // 00bedd7b
+        MOV EAX,dword ptr [EBX + 0xc] // 00bedd7d
+        MOV dword ptr [EDI + 0x4],ECX // 00bedd80
+        PUSH EBX // 00bedd83
+        LEA ECX,[ESP + 0x28] // 00bedd84
+        PUSH ECX // 00bedd88
+        MOV dword ptr [EDI + 0x8],EDX // 00bedd89
+        MOV ECX,EDI // 00bedd8c
+        MOV dword ptr [EDI + 0xc],EAX // 00bedd8e
+        CALL subtract_raw // 00bedd91
+        MOV EDX,dword ptr [EAX] // 00bedd96
+        MOV dword ptr [ESI + 0x40],EDX // 00bedd98
+        MOV ECX,dword ptr [EAX + 0x4] // 00bedd9b
+        MOV dword ptr [ESI + 0x44],ECX // 00bedd9e
+        MOV EDX,dword ptr [EAX + 0x8] // 00bedda1
+        MOV dword ptr [ESI + 0x48],EDX // 00bedda4
+        MOV EAX,dword ptr [EAX + 0xc] // 00bedda7
+        MOV dword ptr [ESI + 0x4c],EAX // 00beddaa
     label_00beddad: POP EDI
-    label_00beddae: POP EBX
-    label_00beddaf: POP ESI
-    label_00beddb0: ADD ESP,0x28
-    label_00beddb3: RET
+        POP EBX // 00beddae
+        POP ESI // 00beddaf
+        ADD ESP,0x28 // 00beddb0
+        RET // 00beddb3
     }
 }
 
